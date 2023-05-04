462dc9ef18e29e4b852e77390907f1b381ce3bae
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2023-05-04 12:01:41 +0200

    finished the about us page

---
+++ client/src/components/AboutUsLocation.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class='text-center  h-80 border border-black'>
+        <h1>Location</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
