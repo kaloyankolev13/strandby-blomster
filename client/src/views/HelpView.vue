704dc1d40aa07ee233ee4d7828f7b921cde37ab5
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2023-05-09 12:44:23 +0200

    Help page

--- client/src/views/HelpView.vue
+++ client/src/views/HelpView.vue
@@ -4,6 +4,11 @@
             <h1 class='text-4xl mb-10'>Help and FAQ</h1>
             <p >To facilitate your order process, we have put together a list of the questions <br> that are often asked by customers. You can find this information under Questions and Answers.</p>
         </div>
+        <div id='dropdown' class=' mx-auto'>
+            <button>What is the difference between bouquet size and price groups?</button>
+            <p>There are 3 different price categories for most of our bouquets. Size varies with price, and includes more or fewer flowers. 
+The image most often represents medium. The exact number of flowers may vary depending on the season and daily changes in prices at the florist, but we guarantee that the colour, shape and amount are always correct!</p>
+        </div>
     </div>
 </template>
 
