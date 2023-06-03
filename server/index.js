20bf272be1a7890dfae0e116e91e1c7f047e5dae
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2023-06-03 17:19:06 +0200

    fix?

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('tiny'));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://strandbyblomster.onrender.com');
+  res.header('Access-Control-Allow-Origin', process.env.VUE_APP_API_URL);
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
